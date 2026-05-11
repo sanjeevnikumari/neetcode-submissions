b163be1a62f4a38a670593ec88653299dfb9165f
sanjeevni kumari <157292826+sanjeevnikumari@users.noreply.github.com>
2026-05-11 09:37:24 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-0

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-0.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q)
+            return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        if (left && right)
+            return root;   // dono mil gaye → LCA
+
+        return left ? left : right;
+    }
+};
