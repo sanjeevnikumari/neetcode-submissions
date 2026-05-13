4de8d7c91f0e593d1c5c776feab8829a13722894
sanjeevni kumari <157292826+sanjeevnikumari@users.noreply.github.com>
2026-05-13 15:08:15 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    
+    bool topologicalSortCheck(unordered_map<int, vector<int>> &adj, int n, vector<int> &indegree) {
+        queue<int> que;
+        int count = 0;
+        for(int i = 0; i<n; i++) {
+            if(indegree[i] == 0) {
+                count++;
+                que.push(i);
+            }
+        }
+        while(!que.empty()) {
+            int u = que.front();
+            que.pop();
+            for(int &v : adj[u]) {
+                indegree[v]--;
+                if(indegree[v] == 0) {
+                    count++;
+                    que.push(v);
+                }
+            }
+        }
+        if(count == n) 
+            return true; 
+        return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> adj;
+        vector<int> indegree(numCourses, 0); 
+        for(auto &vec : prerequisites) {
+            int a = vec[0];
+            int b = vec[1];
+            adj[b].push_back(a);
+            indegree[a]++;
+        }        
+        return topologicalSortCheck(adj, numCourses, indegree);
+    }
+};
